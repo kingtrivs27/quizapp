0146ba5fbf023a7d25074a148797f9169edf45db
MRINAL TRIVEDI <kingtrivs27@gmail.com>
2016-03-11 00:57:11 +0530

    cleaned git cache

--- app/controllers/api/v1/subjects_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V1::SubjectsController < Api::ApiController
-
-  def get_subjects
-  #   todo check is current user is there using the access token
-
-    all_subjects = Subject.select(:id, :name).all
-    render json: get_v1_formatted_response({all_subjects: all_subjects}, true, [])
-  end
-end
--- db/migrate/20160308221630_add_column_image_url_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnImageUrlToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :image_url, :string, default: nil
-  end
-end
