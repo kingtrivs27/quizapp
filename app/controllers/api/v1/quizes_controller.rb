e4b7cff46561a713c2ab6e005adbaac3554fe44e
MRINAL TRIVEDI <kingtrivs27@gmail.com>
2016-03-12 05:22:02 +0530

    quiz request commit transaction before sending notifiation

--- app/controllers/api/v1/quizes_controller.rb
+++ app/controllers/api/v1/quizes_controller.rb
@@ -6,58 +6,66 @@ class Api::V1::QuizesController < Api::ApiController
     current_user = @current_user || User.find_by(api_key: params[:access_token])
     quiz_response = {}
 
-    ActiveRecord::Base.transaction do
-      # TODO::Avoid race condition in future for this request
-      pending_request = Quiz.where(subject_id: requested_subject_id).pending.limit(1).first
-      quiz_service = QuizService.new(current_user)
 
-      if pending_request.present?
-        if params[:mode] == 'bot'
+    # TODO::Avoid race condition in future for this request
+    pending_request = Quiz.where(subject_id: requested_subject_id).pending.limit(1).first
+    quiz_service = QuizService.new(current_user)
 
+    if pending_request.present?
+      if params[:mode] == 'bot'
+
+        ActiveRecord::Base.transaction do
           pending_request.update_attributes({opponent_type: 1, status: 1, opponent_id: 0, requester_waiting: false })
+        end
 
-          first_user_payload = {
-            type: 'QUIZ_START',
-            name: 'Bot' ,
-            facebook_id: 0
-          }
+        first_user_payload = {
+          type: 'QUIZ_START',
+          name: 'Bot' ,
+          facebook_id: 0
+        }
 
-          gcm_response = send_notification(first_user_payload, Device.where(user_id: current_user.id).pluck(:google_api_key))
-          Rails.logger.info("##########################################")
-          Rails.logger.info(gcm_response)
-          Rails.logger.info("##########################################")
-        else
+        gcm_response = send_notification(first_user_payload, Device.where(user_id: current_user.id).pluck(:google_api_key))
+        Rails.logger.info("##########################################")
+        Rails.logger.info(gcm_response)
+        Rails.logger.info("##########################################")
+      else
+        ActiveRecord::Base.transaction do
           quiz_response = quiz_service.add_as_opponent(pending_request)
+
           if quiz_response[:success]
-            # send notification to both users
-            first_user_payload = {
-              type: 'QUIZ_START',
-              name: current_user.name ,
-              facebook_id: current_user.facebook_id
-            }
-
-            first_user = User.find_by(id: pending_request.requester_id)
-            second_user_payload = {
-              type: 'QUIZ_START',
-              name: first_user.name,
-              facebook_id: first_user.facebook_id
-            }
-
-            gcm_response = send_notification(second_user_payload, Device.where(user_id: current_user.id).pluck(:google_api_key))
-            Rails.logger.info("##########################################")
-            Rails.logger.info(gcm_response)
-            Rails.logger.info("##########################################")
-
-            gcm_response = send_notification(first_user_payload, Device.where(user_id: first_user.id).pluck(:google_api_key))
-            Rails.logger.info("##########################################")
-            Rails.logger.info(gcm_response)
-            Rails.logger.info("##########################################")
-
             pending_request.status = Quiz.statuses[:started]
             pending_request.save
           end
         end
-      else
+
+        if quiz_response[:success]
+          # send notification to both users
+          first_user_payload = {
+            type: 'QUIZ_START',
+            name: current_user.name ,
+            facebook_id: current_user.facebook_id
+          }
+
+          first_user = User.find_by(id: pending_request.requester_id)
+          second_user_payload = {
+            type: 'QUIZ_START',
+            name: first_user.name,
+            facebook_id: first_user.facebook_id
+          }
+
+          gcm_response = send_notification(second_user_payload, Device.where(user_id: current_user.id).pluck(:google_api_key))
+          Rails.logger.info("##########################################")
+          Rails.logger.info(gcm_response)
+          Rails.logger.info("##########################################")
+
+          gcm_response = send_notification(first_user_payload, Device.where(user_id: first_user.id).pluck(:google_api_key))
+          Rails.logger.info("##########################################")
+          Rails.logger.info(gcm_response)
+          Rails.logger.info("##########################################")
+        end
+      end
+    else
+      ActiveRecord::Base.transaction do
         quiz_response = quiz_service.create_pending_quiz(quiz_params)
       end
     end
