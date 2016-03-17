d7928d784e10d2c1c3c407f742eeb061fe4e65a2
MRINAL TRIVEDI <kingtrivs27@gmail.com>
2016-03-18 01:17:24 +0530

    drawer response

--- app/controllers/api/v1/navigations_controller.rb
+++ app/controllers/api/v1/navigations_controller.rb
@@ -2,14 +2,12 @@ class Api::V1::NavigationsController < Api::ApiController
 
   def drawer_menu
     response = {
-      drawer_menu: {
         drawer: [
           {
             text: 'About Us',
             uri: 'cacpt://webview/?au=http://ec2-54-187-93-74.us-west-2.compute.amazonaws.com/v2/about_us'
           }
         ]
-      }
     }
     render json: get_v1_formatted_response(response, true, ['success'])
   end
